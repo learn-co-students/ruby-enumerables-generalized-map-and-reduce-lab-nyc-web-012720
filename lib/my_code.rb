ab49714102b975fdfe2a3b8ec199d566289dddc1
Adit P Patel <app2116@gmail.com>
2020-01-14 21:17:44 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+
+def map (source)
+  result = []
+  i = 0
+  while i < source.length do
+    result.push(yield(source[i]))
+    i += 1
+  end
+  return result
+end
+
+def reduce (source, starting_point = nil)
+  if starting_point 
+     result = starting_point
+     i = 0
+    else 
+     result = source[0]
+     i = 1
+  end
+  
+  while i < source.length do
+    result = yield(result, source[i])
+    i += 1
+  end
+  return result
+  
+end
