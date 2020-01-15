ea50082a337776555ae035daab2cf2162376db73
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-15 02:19:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# Your Code Here
+require 'pry'
+
+def map(array)
+  result = []
+  counter = 0 
+  
+  while counter < array.length do
+    result << yield(array[counter])
+    counter += 1
+  end
+  result
+end
+
+
+def reduce(array, result = 0)
+  counter = 0 
+  
+  while counter < array.length do
+    if array[counter] == true
+      result = true
+    elsif array[counter] == false
+      result = false
+    else
+      result = yield(result, array[counter]) 
+    end 
+    counter += 1
+  end
+  result
+end
