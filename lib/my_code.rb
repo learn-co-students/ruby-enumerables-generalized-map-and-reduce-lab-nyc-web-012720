9fff73d6e3a7d30f9103870602e33f79136804bf
Lee McCormick <lzm0010@gmail.com>
2020-01-24 19:41:01 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  i = 0
+  while i < array.size do
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end
+
+
+def reduce(array, sp=nil)
+  if sp
+    total = sp
+    i = 0
+  else
+    total = array[0]
+    i = 1
+  end
+  
+  while i < array.size do
+    total = yield(total, array[i])
+    i += 1
+  end
+  total
+end
