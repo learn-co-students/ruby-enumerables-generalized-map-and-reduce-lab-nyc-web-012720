9c70843e3a0483c5e3b90aa7a59e673748ee156d
Muhan Lee <davidmlee4@gmail.com>
2020-01-24 05:45:02 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new << (yield(source_array[i]))
+    #unique code
+    i += 1
+  end
+  new
+end
+
+
+def reduce(source_array, sv=nil)
+  if sv
+    total = sv
+    i = 0
+  else
+    total = source_array[0]
+    i = 1
+  end
+  while i < source_array.length do
+    total = yield(total, source_array[i])
+    i += 1
+  end
+  total
+end
